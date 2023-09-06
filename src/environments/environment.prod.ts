e3cb6bae3c41d51cf4bb54bdfd3653175ad26d8f
Juan Pablo Sánchez Rey <juanpablosanchezrey22@gmail.com>
2023-09-06 17:06:59 -0500

    Change URL for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  URL_API: 'http://localhost:8080/api'
+  URL_API: 'https://nexuschallengebackend-production.up.railway.app/api'
+  // URL_API: 'http://localhost:8080/api'
 };
 
